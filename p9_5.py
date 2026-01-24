7f9814874e73302a247c79dfbb57f216e35f1b1c
Nick <tsibulko.itstep@gmail.com>
2026-01-24 08:28:38 +0200

    pars

---
+++ p9_5.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://coinmarketcap.com/")
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    soup_list = soup.find_all("div", {"class": "sc-631098c-0"})
+    for item in soup_list:
+        print(item.text)
